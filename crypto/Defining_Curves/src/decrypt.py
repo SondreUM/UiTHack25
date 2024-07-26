7853cc4e9607ce3d91220eb1a1e8935d6ef781af
frapzo <nikolai.lorentzen@gmail.com>
2024-07-26 15:14:21 +0200

    curve now matches Curvy Fascination

--- crypto/Defining_Curves/src/decrypt.py
+++ crypto/Defining_Curves/src/decrypt.py
@@ -1,11 +1,10 @@
 from hashlib import sha256
 from Crypto.Cipher import AES
-from binascii import *
 
-key = input("key: ").encode()
-ciphertext = b'W\xc7\xd2\x8e\xc0\x9d\x1bYi}\x19\xbf\xa6\xb9l\xf7\xae\x9f\x89,]\x8e\xc3\xd7\x17\xa41\xb2\xa4\xe9z\xb6\xbd'
+key = input("type/paste key: ").encode()
+ciphertext = b'"t\xdd\xfb\x1b\xdds\xf5\x81\'\xf6@\xcbF\xcd\xff\x1e\xaa\xa93\x9e\rx{?\x18K\r\x11~\xca]X'
 
-nonce = b'J\x06g\xf1\x97\xb1\x92G\x03\x86\x85\xf6i\x1c\xe0\x88'
+nonce = b'.\x00\x91\x9c]\xc0\xe4+f=\xa4\x90\xfd\x0b&\xb5'
 AESkey = sha256(key).digest()
 cipher = AES.new(AESkey, AES.MODE_EAX, nonce=nonce)
 plain = cipher.decrypt(ciphertext).decode()
