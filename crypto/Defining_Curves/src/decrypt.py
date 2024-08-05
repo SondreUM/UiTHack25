d700e6c3b9cd82fd374c171d5cce198d478d406c
frapzo <nikolai.lorentzen@gmail.com>
2024-08-05 09:08:01 +0200

    somehow the curve was not updated to the Curvy Fasciantion one??

--- crypto/Defining_Curves/src/decrypt.py
+++ crypto/Defining_Curves/src/decrypt.py
@@ -2,9 +2,9 @@ from hashlib import sha256
 from Crypto.Cipher import AES
 
 key = input("type/paste key: ").encode()
-ciphertext = b'"t\xdd\xfb\x1b\xdds\xf5\x81\'\xf6@\xcbF\xcd\xff\x1e\xaa\xa93\x9e\rx{?\x18K\r\x11~\xca]X'
+ciphertext = b'\xee\x0e=\xd1T\xe9}\xec\x11A\n\xf2\xb8\x13\x06\x08[{\x05L\xceC\t-g\x83\xc1iJ\xc6\x1eDW'
 
-nonce = b'.\x00\x91\x9c]\xc0\xe4+f=\xa4\x90\xfd\x0b&\xb5'
+nonce = b'\xdc-A\x8b\x00r\xe3\x9d\x92`\x9bKE\xa3\x0b-'
 AESkey = sha256(key).digest()
 cipher = AES.new(AESkey, AES.MODE_EAX, nonce=nonce)
 plain = cipher.decrypt(ciphertext).decode()
