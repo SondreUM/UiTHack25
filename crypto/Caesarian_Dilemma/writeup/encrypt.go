74ec6b0e515210c9820a4b095a8ab28cc6685b54
Nikolai <70200189+frapzo@users.noreply.github.com>
2024-09-02 14:30:28 +0200

    challenge(crypto): Caesarian dilemma (#14)
    * added challenge Caesarian dilemma
    
    * updated challenge.yml to reflect template
    
    * added writeup
    
    * added decrypted flag to writeup
    
    * added description in theme
    
    * added description to yaml and added challenge to root readme
    
    * Update crypto/Caesarian_Dilemma/writeup/README.md
    
    Co-authored-by: Sondre <72896681+SondreUM@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Sondre <72896681+SondreUM@users.noreply.github.com>

---
+++ crypto/Caesarian_Dilemma/writeup/encrypt.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func encrypt(input string, shift int) string {
+	var alphabet_lower string = "abcdefghijklmnopqrstuvwxyz"
+	var alphabet_upper string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	var ciphertext string = ""
+	var index int
+
+	for _, char := range input {
+		if 'a' <= char && char <= 'z' {
+			// lower case
+			index = strings.IndexRune(alphabet_lower, char)
+			index = (index + shift) % 26
+			ciphertext += string(alphabet_lower[index])
+		} else if 'A' <= char && char <= 'Z' {
+			// upper case
+			index = strings.IndexRune(alphabet_upper, char)
+			index = (index + shift) % 26
+			ciphertext += string(alphabet_upper[index])
+		} else {
+			// non-letter
+			ciphertext += string(char)
+		}
+	}
+	return ciphertext
+}
+
+func main() {
+	// allocate space for flag
+	var byteFlag = make([]byte, 100)
+	var flag string
+	var err error
+
+	// Read flag from file
+	file, err := os.Open("../src/flag.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	var read, _ = file.Read(byteFlag)
+	defer file.Close()
+
+	// Convert flag to string and encrypt it
+	flag = string(byteFlag[:read])
+	var shift int = 15
+	var ciphertext string = encrypt(flag, shift)
+
+	// Create file to store the encrypted flag
+	var filename string = "../src/flag.txt.enc"
+	file, err = os.Create(filename)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Write encryptet flag to file
+	_, err = file.WriteString(ciphertext)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer file.Close()
+}
