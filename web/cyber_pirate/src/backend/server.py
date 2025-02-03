669fa7a2796071d4f3e767e97aa8e62d211ee933
ilyaTTT <ilya2000@live.no>
2025-02-03 09:09:21 +0100

    fixed up most of it, has few questions

--- web/cyber_pirate/src/backend/server.py
+++ web/cyber_pirate/src/backend/server.py
@@ -71,4 +71,4 @@ def login():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, threaded=True)
+    app.run(host="0.0.0.0", port=8006, threaded=True)
--- web/cyber_pirate/src/frontend/app/LoginForm.js
+++ web/cyber_pirate/src/frontend/app/LoginForm.js
@@ -13,7 +13,7 @@ function LoginForm() {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://uithack.td.org.uit.no:5001/api/login", {
+      const response = await fetch("https://uithack-2.td.org.uit.no:8006/api/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/x-www-form-urlencoded",
