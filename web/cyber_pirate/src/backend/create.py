aba9443c2bfc781b1995872e2e0533d5f5495925
SondreUM <sondreum@gmail.com>
2025-02-05 14:14:02 +0100

    refactor(cyber_pirate): update flag to correct version

--- web/cyber_pirate/src/backend/create.py
+++ web/cyber_pirate/src/backend/create.py
@@ -1,8 +1,10 @@
 import sqlite3
 
-conn = sqlite3.connect('database.sqlite')
+conn = sqlite3.connect("database.sqlite")
 c = conn.cursor()
-c.execute('''CREATE TABLE secret (key TEXT, flag TEXT)''')
-c.execute("INSERT INTO secret (key, flag) VALUES ('asdfmjpagy0123jgla09+213kasgf032', 'UiTHack24{DaTaBaseS_Expl0it4t10n}')")
+c.execute("""CREATE TABLE secret (key TEXT, flag TEXT)""")
+c.execute(
+    "INSERT INTO secret (key, flag) VALUES ('asdfmjpagy0123jgla09+213kasgf032', 'UiTHack25{DaTaBaseS_Expl0it4t10n}')"
+)
 conn.commit()
 conn.close()
