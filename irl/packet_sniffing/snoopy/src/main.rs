bbf5fc66b69bb76b9502b8f12da6616481d80c5b
Victor Zimmer <victor.zimmer@me.com>
2025-02-28 19:35:50 +0100

    Added basic application that broadcasts a flag over UDP

---
+++ irl/packet_sniffing/snoopy/src/main.rs
@@ -0,0 +1,23 @@
+use std::time::Duration;
+use std::{net::UdpSocket, thread::sleep};
+
+fn main() -> ! {
+    let socket: UdpSocket = UdpSocket::bind("0.0.0.0:34254").expect("Failed to bind socket");
+    socket
+        .set_read_timeout(Some(Duration::new(5, 0)))
+        .expect("Failed to set read timeout");
+    socket.set_broadcast(true).expect("Failed to set broadcast");
+    println!("Connected on port {}", 25550);
+    println!("Broadcast: {:?}", socket.broadcast());
+    println!("Timeout: {:?}", socket.read_timeout());
+
+    let flag = String::from("UiTHack{example_flag}");
+
+    loop {
+        socket
+            .send_to(flag.as_bytes(), "255.255.255.255:0")
+            .expect("Failed to send flag");
+
+        sleep(std::time::Duration::from_millis(10000));
+    }
+}
